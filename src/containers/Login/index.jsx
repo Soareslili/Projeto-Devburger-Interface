76d29b70d92f83dad264496a74fc5dc8a985bb1b
Soareslili <lidianesantossoares2@gmail.com>
2025-03-25 14:24:40 -0300

    Instalado React Hook Form, e iniciando validação do formulario

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,39 +1,62 @@
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+
 import { Container, InpuntContainer, LeftContainer, RightConatiner, Title, Form } from "./styles";
 import Logo from '../../assets/logo.svg'
 import { Button } from "../../components/Button";
 
 
-export function Login(){
+export function Login() {
+
+    const schema = yup
+        .object({
+            email: yup.string().email().required(),
+            password: yup.string().min(6).required(),
+        })
+        .required()
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    })
+    const onSubmit = (data) => console.log(data)
+
+
     return (
         <Container>
-           <LeftContainer>
-            <img src={Logo} alt="logo-devburger"/>
-           </LeftContainer>
-           
-           <RightConatiner>
-            <Title>
-            Olá, seja bem vindo ao <span> Dev Burguer! </span>
-            <br/>
-            Acesse com seu <span> Login e senha. </span> 
-            </Title>
-            <Form>
-                <InpuntContainer>
-                    <label>Email</label>
-                    <input type="email"/>
-                </InpuntContainer>
+            <LeftContainer>
+                <img src={Logo} alt="logo-devburger" />
+            </LeftContainer>
 
-                <InpuntContainer>
-                    <label>Senha</label>
-                    <input type="password"/>
-                </InpuntContainer>
+            <RightConatiner>
+                <Title>
+                    Olá, seja bem vindo ao <span> Dev Burguer! </span>
+                    <br />
+                    Acesse com seu <span> Login e senha. </span>
+                </Title>
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    <InpuntContainer>
+                        <label>Email</label>
+                        <input type="email" {...register("email")}/>
+                    </InpuntContainer>
 
-                <Button>Entrar</Button>
+                    <InpuntContainer>
+                        <label>Senha</label>
+                        <input type="password" {...register("password")}/>
+                    </InpuntContainer>
 
-            </Form>
+                    <Button type="submit" >Entrar</Button>
 
-            <p>Não possui conta? <a>Clique aqui.</a></p>
-           
-           </RightConatiner>
+                </Form>
+
+                <p>Não possui conta? <a>Clique aqui.</a></p>
+
+            </RightConatiner>
         </Container>
     );
 }
